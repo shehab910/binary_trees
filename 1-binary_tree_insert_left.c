857228de47830e05c9d469c650a80a551cf1b638
shehab910 <shehabihab@hotmail.com>
2024-05-05 09:53:55 +0300

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,17 +3,17 @@
 /**
   * binary_tree_insert_left - Insert new node as the left child of another node
   *
-  * @p: Pointer to the node to insert the left child in
-  * @v: Value to be stored in the new node
+  * @parent: Pointer to the node to insert the left child in
+  * @value: Value to be stored in the new node
   * Return: Pointer to the created node on success,
-  * NULL on failure or if p is NULL
+  * NULL on failure or if parent is NULL
   */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *p, int v)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *nn;
 
-	if (p == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
@@ -23,20 +23,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *p, int v)
 	{
 		return (NULL);
 	}
-	if (p->left != NULL)
+	if (parent->left != NULL)
 	{
-		nn->left = p->left;
-		nn->left->p = nn;
-		nn->p = p;
-		p->left = nn;
+		nn->left = parent->left;
+		nn->left->parent = nn;
+		nn->parent = parent;
+		parent->left = nn;
 	}
 	else
 	{
-		p->left = nn;
-		nn->p = p;
+		parent->left = nn;
+		nn->parent = parent;
 		nn->left = NULL;
 	}
-	nn->n = v;
+	nn->n = value;
 	nn->right = NULL;
 	return (nn);
 }
